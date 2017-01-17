3a020a7eaa7a26d2f57356479af62f5fee89bd30
Маннанов Ильдар Фаварисович <IFMannanov@sberbank.ru>
2017-01-17 20:15:37 +0300

    изменения в рамках урока 1

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+class Article extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    render () {
+        const {article} = this.props;
+        const body = this.state.isOpen ? <p>{article.text}</p> : null
+        return(
+            <section>
+                <h3 onClick={this.handleClick}>{article.title}</h3>
+                {body}
+            </section>
+        )
+    }
+
+    handleClick = ev => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+export default Article
+/*
 export default props => {
     const {article} = props;
     return(
@@ -9,3 +38,5 @@ export default props => {
         </section>
     )
 }
+
+*/
